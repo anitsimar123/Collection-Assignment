0acc1cd33011e1665a31e9d843e1f1b8fd74b767
amritpal <amritpal.singh@tothenew.com>
2022-10-02 21:55:47 +0530

    Initial Commit

---
+++ Java9_17Features/src/que1/Main.java
@@ -0,0 +1,8 @@
+package que1;
+
+public class Main {
+    public static void main(String[] args) {
+                intrfc.newMethod();
+                intrfc.anotherMethod();
+    }
+}
---
+++ Java9_17Features/src/que1/intrfc.java
@@ -0,0 +1,15 @@
+package que1;
+
+public interface intrfc {
+        private static void privateMethod(){
+            System.out.println("Private Method called");
+        }
+        public static void newMethod(){
+            privateMethod();
+            System.out.println("Method 1");
+        }
+        public static void anotherMethod(){
+            privateMethod();
+            System.out.println("Method 2");
+        }
+}
